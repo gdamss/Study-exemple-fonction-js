5858cf14579b5633f972deeea60b23e2554b1d68
gdamss <elbryan@hotmail.fr>
2025-02-27 19:11:36 +0100

    suite cours

--- exemple_3_fonction.js
+++ exemple_3_fonction.js
@@ -1,12 +1,39 @@
-let nombre = 100;
+// let nombre = 100;
 
-function calcDouble() {
-  let double = nombre * 2;
-  console.log(" Le double du nombre " + nombre + " est " + double);
+// function calcDouble(nb) {
+//   let double = nb * 2;
+//   console.log(" Le double du nombre " + nb + " est " + double);
+// }
+
+// calcDouble(100);
+
+// calcDouble(40);
+
+// function perimetreRectangle(largeur, longueur) {
+//   let perimetre = 2 * largeur + 2 * longueur;
+//   return perimetre;
+// console.log(" Le peremetre du rectangle est de " + perimetre + " cm2 ");
+// }
+
+// perimetreRectangle(4, 5);
+
+// perimetreRectangle(2, 4);
+
+// perimetreRectangle(1, 2);
+
+// perimetreRectangle(18, 20);
+
+// console.log(perimetreRectangle(4, 5));
+
+// console.log(perimetreRectangle(8, 5));
+
+// console.log(perimetreRectangle(4.4, 1.67));
+
+// console.log(perimetreRectangle(64, 45));
+
+function calcHypotenuse(a, b) {
+  let hypotenuse = Math.sqrt(a ** 2 + b ** 2);
+  return hypotenuse;
 }
 
-calcDouble();
-
-nombre = 40;
-
-calcDouble();
+console.log("Hypotenuse : " + calcHypotenuse(10, 11));
